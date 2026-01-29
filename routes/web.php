dc33bee9df49819196b0dc3884f5aec3bdd8fee6
Nur <nurrachman@gmail.com>
2026-01-29 15:51:14 +0700

    feat: implementasi method POST untuk input data movie ke array

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+    $movies = [];
+
+    for ($i = 0 ; $i < 10; $i++){
+        $movies[]=[
+            'title' => 'Movie',
+            'year' => '2025',
+            'genre' => 'Action',
+        ];
+    }
+
 Route::get('/movie', function () {
     $movies = [];
 
@@ -27,6 +37,19 @@ Route::get('/movie', function () {
     echo '</ul>';
 });
 
-Route::post('/movie', function() {
-    return request()->all();
+Route::post('/movie', function() use ($movies){
+    $movies[] = [
+        'title' => request('title'),
+        'genre' => request('genre'),
+        'year' => request('year'),
+    ];
+
+    echo '<h1>Movies</h1>';
+    echo '<ul>';
+
+    foreach($movies as $movie) {
+        echo '<li>' .$movie['title'] . '-' . $movie['year'] . '-'. $movie['genre'] . '</li>';
+    }
+
+    echo '</ul>';
 });
