dcfa0bc430d294e8374fb2614bf2e58753c7f06f
Nur <nurrachman@gmail.com>
2026-01-29 14:54:48 +0700

    add : menambahkan fitur perulangan di file routes\web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/movie', function () {
+    $movies = [];
+
+    for ($i = 0 ; $i < 10; $i++){
+        $movies[]=[
+            'title' => 'Movie',
+            'year' => '2025',
+            'genre' => 'Action',
+        ];
+    }
+
+    echo '<h1>Movies</h1>';
+    echo '<ul>';
+
+    foreach($movies as $movie) {
+        echo '<li>' .$movie['title'] . '-' . $movie['year'] . '-'. $movie['genre'] . '</li>';
+    }
+
+    echo '</ul>';
+});
