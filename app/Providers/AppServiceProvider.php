416ab3a53cba55edb3125e332f746e9d8c21b7ad
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-02-06 15:34:53 +0700

    membagikan data menggunakan view Composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,18 +21,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         
-        View::share('menu', [
-            'Home' => '/',
-            'About' => '/about',
-            'Contact' => '/contact',
-        ]);
-
-        // View::composer(['*'], function ($view){
-        //     $view->with('menu', [
+        // View::share('menu', [
         //     'Home' => '/',
         //     'About' => '/about',
         //     'Contact' => '/contact',
-        //     ]);
-        // });
+        // ]);
+
+        View::composer(['*'], function ($view){
+            $view->with('menu', [
+            'Home' => '/',
+            'About' => '/about',
+            'Contact' => '/contact',
+            ]);
+        });
     }
 }
