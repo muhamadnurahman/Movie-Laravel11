139dfff0e1e08ef5ea2618a0c1ace89e351dd6fa
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-02-14 01:30:47 +0700

    Validation - Mengembalikan Nilai Inputan Sebelumnya

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -11,42 +11,47 @@
             @csrf
             <div>
                 <label for="title" class="block text-lg mb-2">Title</label>
-                <input type="text" name="title" id="title" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('title') border-red-500 @enderror">
+                <input type="text" name="title" id="title" value="{{ old('title') }}" 
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('title') border-red-500 @enderror">
                 @error('title')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div>
                 <label for="description" class="block text-lg mb-2">Description</label>
-                <textarea name="description" id="description" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('description') border-red-500 @enderror"></textarea>
+                <textarea name="description" id="description" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('description') border-red-500 @enderror">{{ old('description') }}</textarea>
                 @error('description')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div>
                 <label for="release_date" class="block text-lg mb-2">Release Date</label>
-                <input type="date" name="release_date" id="release_date" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('release_date') border-red-500 @enderror">
+                <input type="date" name="release_date" id="release_date" value="{{ old('release_date') }}"
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('release_date') border-red-500 @enderror">
                 @error('release_date')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div>
                 <label for="cast" class="block text-lg mb-2">Cast</label>
-                <input type="text" name="cast" id="cast" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('cast') border-red-500 @enderror">
+                <input type="text" name="cast" id="cast" value="{{ old('cast') }}"
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('cast') border-red-500 @enderror">
                 @error('cast')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div>
                 <label for="genres" class="block text-lg mb-2">Genres</label>
-                <input type="text" name="genres" id="genres" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('genres') border-red-500 @enderror">
+                <input type="text" name="genres" id="genres" value="{{ old('genres') }}"
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('genres') border-red-500 @enderror">
                 @error('genres')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div>
                 <label for="image" class="block text-lg mb-2">Poster</label>
-                <input type="text" name="image" id="image" class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('poster') border-red-500 @enderror">
+                <input type="text" name="image" id="image" value="{{ old('image') }}" 
+                class="w-full p-2 bg-gray-800 border border-gray-700 rounded focus:outline-none focus:ring-2 focus:ring-blue-600 @error('poster') border-red-500 @enderror">
                 @error('image')
                 <p class="text-red-500">{{ $message }}</p>
                 @enderror
