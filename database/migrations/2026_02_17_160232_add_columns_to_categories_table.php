856135bf26c8d0231121b930c5f246b399f339ef
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-02-17 23:11:33 +0700

    Menambahkan Kolom Baru Pada Tabel

---
+++ database/migrations/2026_02_17_160232_add_columns_to_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Extension\DescriptionList\Node\Description;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            //
+            $table->boolean('is_active')->default(true);
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            //
+            $table->dropColumn(['is_active', 'description']);
+        });
+    }
+};
