b66489a54865ed52f915fd291fe1c5448e1f8321
muhamadnurahman <muhamadnurahman2006@gmail.com>
2026-02-18 00:52:11 +0700

    Mendapatkan Data dengan Eloquent

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,17 @@ class CategoryController extends Controller
         // $categories = DB::table('categories')->select(['id', 'name', 'slug'])->get(); 
         // $categories = DB::table('categories')->whereIn('id', [1, 2, 5])->get(); 
         // $categories = DB::table('categories')->where('id', 1)->first(); 
-        $categories = DB::table('categories')
-            ->where('id', 1)
-            ->select(['id', 'name', 'slug'])->first(); 
+        // $categories = DB::table('categories')
+        //     ->where('id', 1)
+        //     ->select(['id', 'name', 'slug'])->first(); 
+
+        // $categories = Category::all();
+        // $categories = Category::whereIn('id', [1, 2, 5])
+        //     ->select(['id', 'name', 'slug'])
+        //     ->get();
+        $categories = Category::where('id', 1)
+            ->select(['id', 'name', 'slug'])
+            ->first();
 
         return $categories;
     }
